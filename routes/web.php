a3d8b5c2bb9a380bd8d0effd207ba497cc0dcd57
Stuart Todd <stuart@Stuarts-MacBook-Air.local>
2022-03-02 21:20:52 +0000

    Create controller

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/', [VistechController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard');
 
 require __DIR__.'/auth.php';
